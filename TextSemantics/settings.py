b9a50b65c5477c68b03e5dbf6b53d629e67d9a1d
Preetam Pati <preetam6teen@gmail.com>
2024-03-31 05:34:12 +0530

    Added vercel deployments - Preetam Pati

--- TextSemantics/settings.py
+++ TextSemantics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@1fqnhsvra%(leq+0qqb=6*lbmg&emdw%0p*%x4w*8mm38m&16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os 
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
